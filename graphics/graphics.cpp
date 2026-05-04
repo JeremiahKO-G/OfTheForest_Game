237831e059bf701d3485346b98cfa33db28c18c2
JeremiahKO-G <crisppaper3@gmail.com>
2026-05-04 14:54:48 -0500

    added back in level 1 tiles

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -64,7 +64,7 @@ int Graphics::get_texture_id(const std::string& image_filename) {
     }
 }
 
-void Graphics::draw_sprite(const Vec<float> &pixel, const Sprite &sprite, bool flash, Color) {
+void Graphics::draw_sprite(const Vec<float> &pixel, const Sprite &sprite, bool flash) {
     if (sprite.texture_id < 0) { // sprite has no texture
         return;
     }
